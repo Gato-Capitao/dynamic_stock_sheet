258641a1c296817a98cd93fc3777a39947c702df
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2023-10-01 07:34:33 +0100

    documentação da função de pegar preço

--- Codes/collector.py
+++ Codes/collector.py
@@ -1,7 +1,12 @@
 from yfinance import Ticker
 
-def get_price(action:Ticker) -> float:
+def get_current_price(action:Ticker) -> float:
+    """
+    action: the action object of type Ticker of the yfinance library that has all the information about the action.
+
+    This function will return the current price of the action in float.
+    """
     price = action.info["currentPrice"]
     return price
 
-print(get_price(Ticker("MXRF11.SA")))
+print(get_current_price(Ticker("MXRF11.SA")))
