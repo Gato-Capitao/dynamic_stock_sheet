13468cb46b5d87170bd6d6f51aa684a47589b145
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2023-10-03 06:47:38 +0100

    Mudando dados da tabela para english

--- Codes/organizer.py
+++ Codes/organizer.py
@@ -4,13 +4,13 @@ from collector import get_current_price
 from yfinance import Ticker
 
 def get_list_of_actions(table:DataFrame) -> deque:
-    return deque(table["Nome"])
+    return deque(table["Name"])
 
 def update_action(table:DataFrame, action_pos:int, action_name:str) -> None:
     connector={
-        "Preço":get_current_price
+        "Price":get_current_price
     }
-    action_ticker = Ticker(action_name+".SA")
+    action_ticker = Ticker(action_name)
 
     for info in connector.keys():
         table.loc[action_pos, info]=connector[info](action_ticker)
--- Codes/runner.py
+++ Codes/runner.py
@@ -5,15 +5,14 @@ path="D:\Codes\Projetos Github\Planilha-de-renda-variavel-inteligente\Codes\data
 name="Sheet1"
 
 table = read_excel(path, sheet_name=name)
-print(table["Preço"])
+print(table["Price"])
 
 if __name__ == "__main__":
-
     deque_actions = get_list_of_actions(table)
 
     for pos, action_name in enumerate(deque_actions):
         update_action(table, pos, action_name)
     
-    print(table["Preço"])
-   
+    print(table["Price"])
 
+    table.to_excel("D:\Codes\Projetos Github\Planilha-de-renda-variavel-inteligente\Codes\data_table1.xlsx", index=False)
