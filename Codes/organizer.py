a8e4104403d6c19cc76adb13ed2c164a7035e3ab
Adson Gabriel <99619952+Gato-Capitao@users.noreply.github.com>
2023-10-05 04:44:56 +0100

    Documentação de funções
    Documentação das funções: get_list_of_actions e update_action

--- Codes/organizer.py
+++ Codes/organizer.py
@@ -3,10 +3,26 @@ from collections import deque
 from collector import get_current_price, get_average, get_currency, get_div_rate, get_div_yields, get_industry_type, get_price_to_earnings, get_sector
 from yfinance import Ticker
 
-def get_list_of_actions(table:DataFrame) -> deque:
-    return deque(table["Name"])
+def get_list_of_actions(table:DataFrame, collum_name:str) -> deque:
+    """
+    This function will return a deque with the name of all actions.
+
+    table: the data frame of pandas from the sheets.
+    collum_name: the name of the collum with the name of all actions.
+    """
+    return deque(table[collum_name])
 
 def update_action(table:DataFrame, action_pos:int, action_name:str) -> None:
+    """
+    This function will update infos of the action on the data frame.
+
+    Args:
+        table: the data frame of pandas from the sheets.
+        action_pos: the Y position of the cell on the data frame.
+        action_name: the name of the action.
+
+    Operation: The code cycles through each key(column) in the connector dictionary and then uses the function to which the column is connected to get the information and add it to the column.
+    """
     connector={
         "Sector":get_sector,
         "Price":get_current_price,
--- Codes/runner.py
+++ Codes/runner.py
@@ -8,7 +8,7 @@ table = read_excel(path, sheet_name=name)
 print(table)
 
 if __name__ == "__main__":
-    deque_actions = get_list_of_actions(table)
+    deque_actions = get_list_of_actions(table, "Name")
 
     for pos, action_name in enumerate(deque_actions):
         update_action(table, pos, action_name)
